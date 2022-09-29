a41ec0a50d7bb61727eeaeaee96ae3f5029189d0
“MyroslavaVat” <vatajok.m@gmail.com>
2022-09-29 22:14:56 +0300

    Resolve last questions

--- TeamI/FormulStore.cs
+++ TeamI/FormulStore.cs
@@ -10,5 +10,95 @@
 
             return sideLength;
         }
+
+        public static bool IsEquiLateralTriangle(double[] sidesLength)
+        {
+            if (sidesLength.Length != 0)
+            {
+                var firstElement = sidesLength[0];
+
+                for (int i = 0; i < sidesLength.Length; i++)
+                {
+                    if (sidesLength[i] != firstElement)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsoscelesTriangle(double[] sidesLength)
+        {
+            if (sidesLength.Length != 0)
+            {
+                var firstElement = sidesLength[0];
+                int iter = 0;
+
+                for (int i = 0; i < sidesLength.Length; i++)
+                {
+                    if (sidesLength[i] == firstElement)
+                    {
+                        iter++;
+                        if (iter == 2)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            return false;
+        }
+
+        public static bool IsRightTriangle(double[] sidesLength)
+        {
+            if (sidesLength[0] == sidesLength[1] + sidesLength[2])
+            {
+                return true;
+            }
+            if (sidesLength[1] == sidesLength[0] - sidesLength[2])
+            {
+                return true;
+            }
+            if (sidesLength[2] == sidesLength[0] - sidesLength[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static double Perimeter(double[] sidesLength)
+        {
+            double perimeter = 0;
+            for (int i = 0; i < sidesLength.Length; i++)
+            {
+                perimeter = perimeter + sidesLength[i];
+            }
+
+            return perimeter;
+        }
+
+        public static double[] PowAll(double[] sidesLength)
+        {
+            double[] sidesLengthPow = new double[sidesLength.Length];
+
+            for (int i = sidesLength.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                sidesLengthPow[j] = Math.Pow(sidesLength[i], 2);
+            }
+
+            return sidesLengthPow;
+        }
+
     }
 }
+
--- TeamI/Program.cs
+++ TeamI/Program.cs
@@ -13,21 +13,17 @@ var xC = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter coordinate y of dot C");
 var yC = Convert.ToDouble(Console.ReadLine());
 
-Coordinate coordinateA = new Coordinate()
-{
-    X = xA, //A (x)
-    Y = yA, //A(,y)
-};
-Coordinate coordinateB = new Coordinate()
-{
-    X = xB,
-    Y = yB,
-};
-Coordinate coordinateC = new Coordinate()
-{
-    X = xC,
-    Y = yC
-};
+Coordinate coordinateA = new Coordinate();
+coordinateA.X = xA;
+coordinateA.Y = yA;
+
+Coordinate coordinateB = new Coordinate();
+coordinateB.X = xB;
+coordinateB.Y = yB;
+
+Coordinate coordinateC = new Coordinate();
+coordinateC.X = xC;
+coordinateC.Y = yC;
 
 var array = new double[]
 {
@@ -39,3 +35,27 @@ var array = new double[]
 Console.WriteLine("Length of AB is : " + array[0].ToString());
 Console.WriteLine("Length of BC is : " + array[1].ToString());
 Console.WriteLine("Length of AC is : " + array[2].ToString() + "\r\n");
+
+Array.Sort(array);
+
+var sidesLength = FormulStore.PowAll(array);
+string isEquiLateralTriangle = FormulStore.IsEquiLateralTriangle(array) ? "" : "NOT";
+string isoscelesTriangle = FormulStore.IsoscelesTriangle(array) ? "" : "NOT";
+string isRightTriangle = FormulStore.IsRightTriangle(sidesLength) ? "" : "NOT";
+double perimeter = FormulStore.Perimeter(array);
+
+
+
+Console.WriteLine("Triangale IS " + isEquiLateralTriangle + " Equilareral");
+Console.WriteLine("Triangale IS " + isoscelesTriangle + " Isosceles");
+Console.WriteLine("Triangale IS " + isRightTriangle + " Right" + "\r\n");
+
+Console.WriteLine("Perimeter: " + perimeter + "\r\n");
+
+for (int i = 0; i < perimeter; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
