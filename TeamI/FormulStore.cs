25899bb9ee2d652a13c54b2dde69e5198c578c7a
“MyroslavaVat” <vatajok.m@gmail.com>
2022-09-29 11:39:46 +0300

    Add new project

---
+++ TeamI/FormulStore.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamI
+{
+    public static class FormulStore
+    {
+        public static double SideLength(Coordinate firstDot, Coordinate secondDot)
+        {
+
+            var sideLength = Math.Sqrt((Math.Pow(secondDot.X, 2) - 2 * secondDot.X * firstDot.X + Math.Pow(firstDot.X, 2)) +
+                (Math.Pow(secondDot.Y, 2) - 2 * secondDot.Y * firstDot.Y + Math.Pow(firstDot.Y, 2)));
+
+            return sideLength;
+        }
+    }
+}
---
+++ TeamI/Program.cs
@@ -0,0 +1,41 @@
+﻿using TeamI;
+
+Console.WriteLine("Enter coordinate x of dot A");
+var xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter coordinate y of dot A");
+var yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter coordinate x of dot B");
+var xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter coordinate y of dot B");
+var yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter coordinate x of dot C");
+var xC = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter coordinate y of dot C");
+var yC = Convert.ToDouble(Console.ReadLine());
+
+Coordinate coordinateA = new Coordinate()
+{
+    X = xA, //A (x)
+    Y = yA, //A(,y)
+};
+Coordinate coordinateB = new Coordinate()
+{
+    X = xB,
+    Y = yB,
+};
+Coordinate coordinateC = new Coordinate()
+{
+    X = xC,
+    Y = yC
+};
+
+var array = new double[]
+{
+    FormulStore.SideLength(coordinateA, coordinateB),
+    FormulStore.SideLength(coordinateB, coordinateC),
+    FormulStore.SideLength(coordinateA, coordinateC)
+};
+
+Console.WriteLine("Length of AB is : " + array[0].ToString());
+Console.WriteLine("Length of BC is : " + array[1].ToString());
+Console.WriteLine("Length of AC is : " + array[2].ToString() + "\r\n");
